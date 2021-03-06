8390c895e5c09b190e3dbe2f6d92c9ecbad5a34a
gagas3113 <gassatrio@gmail.com>
2021-03-06 14:29:51 +0700

    Update ask.php

--- ask.php
+++ ask.php
@@ -51,7 +51,7 @@
         </div>
         <div class="footer">
             <div class="footer-nav">
-                <a href="#">Github</a>
+                <a href="https://github.com/gagas3113/tugas2_web">Github</a>
             </div>
         </div>
 
